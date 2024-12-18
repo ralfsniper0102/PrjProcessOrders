a497dbfa8f93eb1328e936e2f4c553e47d0a116f
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-12-17 23:55:49 -0300

    feat: creating Messaging layer

---
+++ ProjProcessOrders.Messaging/RabbitMqClientService.cs
@@ -0,0 +1,292 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using ProjProcessOrders.Messaging.DTOs;
+using ProjProcessOrders.UseCase.Enums;
+using ProjProcessOrders.UseCase.UseCases.CreateClient;
+using ProjProcessOrders.UseCase.UseCases.CreateOrder;
+using ProjProcessOrders.UseCase.UseCases.CreateProduct;
+using ProjProcessOrders.UseCase.UseCases.DeleteClient;
+using ProjProcessOrders.UseCase.UseCases.DeleteProduct;
+using ProjProcessOrders.UseCase.UseCases.GetClientById;
+using ProjProcessOrders.UseCase.UseCases.GetClients;
+using ProjProcessOrders.UseCase.UseCases.GetProductById;
+using ProjProcessOrders.UseCase.UseCases.GetProducts;
+using ProjProcessOrders.UseCase.UseCases.UpdateClient;
+using ProjProcessOrders.UseCase.UseCases.UpdateProduct;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using ProjProcessOrders.UseCase.UseCases.DeleteOrder;
+using ProjProcessOrders.UseCase.UseCases.GetListOrderByClient;
+using ProjProcessOrders.UseCase.UseCases.GetOrderById;
+using ProjProcessOrders.UseCase.UseCases.GetOrders;
+using ProjProcessOrders.UseCase.UseCases.GetOrderValue;
+using ProjProcessOrders.UseCase.UseCases.GetQtOrderByClient;
+using ProjProcessOrders.UseCase.UseCases.UpdateOrder;
+
+namespace ProjProcessOrders.Messaging
+{
+    public class RabbitMqClientService
+    {
+        private readonly IModel _channel;
+        private readonly IConfiguration _configuration;
+        private static Dictionary<string, List<ChunkMessageResponse>> _chunksBuffer = new();
+        private readonly Serilog.ILogger _logger;
+        private readonly string _responseQueue;
+
+
+        public RabbitMqClientService(IModel channel, IConfiguration configuration, Serilog.ILogger logger)
+        {
+            _channel = channel;
+            _configuration = configuration;
+            _logger = logger;
+            _channel.QueueDeclare(queue: _configuration["RabbitMqSettings:RequestQueueName"],
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            _responseQueue = configuration["RabbitMqSettings:ResponseQueueName"];
+        }
+
+        public async Task<string> SendMessageInChunksAsync<T>(T request, string correlationId)
+        {
+            byte[] messageBytes = SerializeMessage(request);
+
+            string responseQueueName = DeclareResponseQueue(correlationId, _responseQueue);
+
+            TaskCompletionSource<string> tcs = InicialConsumer(responseQueueName, _channel);
+
+            return await SendMessageInChunk(request, correlationId, messageBytes, responseQueueName, tcs);
+        }
+
+        private byte[] SerializeMessage<T>(T request)
+        {
+            _logger.Information("Serialização da mensagem...");
+
+            var serializedRequest = JsonConvert.SerializeObject(request);
+            return Encoding.UTF8.GetBytes(serializedRequest);
+        }
+
+        private string DeclareResponseQueue(string correlationId, string responseQueue)
+        {
+            _logger.Information("Definição da fila de resposta...");
+            var responseQueueName = $"{responseQueue}_{correlationId}";
+
+            _logger.Information("Declaração da fila de resposta...");
+            var args = new Dictionary<string, object>
+            {
+                { "x-message-ttl", 15000 } 
+            };
+
+            _channel.QueueDeclare(queue: responseQueueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: true,
+                                 arguments: null);
+            return responseQueueName;
+        }
+
+        private TaskCompletionSource<string> InicialConsumer(string responseQueueName, IModel channel)
+        {
+            _logger.Information("Declaração do consumidor...");
+            var consumer = new EventingBasicConsumer(channel);
+            var tcs = new TaskCompletionSource<string>();
+
+            _logger.Information("Inicia o consumo das mensagens...");
+            consumer.Received += (model, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                _logger.Information("Deserialização da mensagem...");
+                ChunkMessageResponse? chunkMessageResponse = DeserializeResponseMessage(message);
+
+                if (chunkMessageResponse == null) //TODO: Testar essa condicao
+                    throw new System.Exception("Mensagem nao pode ser desserializada");
+
+                var correlationId = ea.BasicProperties.CorrelationId;
+                if (!_chunksBuffer.ContainsKey(correlationId))
+                    _chunksBuffer[correlationId] = new List<ChunkMessageResponse>();
+
+                _logger.Information("Adicionando chunk ao buffer...");
+                _chunksBuffer[correlationId].Add(chunkMessageResponse);
+
+                _logger.Information("Verifica se todos os chunks foram recebidos...");
+                if (_chunksBuffer[correlationId].Count == chunkMessageResponse.TotalChunks)
+                {
+                    _logger.Information("Todos os chunks foram recebidos...");
+
+                    var fullMessage = string.Concat(_chunksBuffer[correlationId]
+                        .OrderBy(chunk => chunk.CurrentChunk)
+                        .Select(chunk => Encoding.UTF8.GetString(chunk.Payload)));
+
+                    var chunkMessageReturn = new ChunkMessageReturn()
+                    {
+                        Body = fullMessage,
+                        StatusCode = chunkMessageResponse.StatusCode
+                    };
+
+                    _logger.Information("Retorno da mensagem...");
+                    tcs.SetResult(JsonConvert.SerializeObject(chunkMessageReturn));
+
+
+                    _logger.Information("retira o correlationId do buffer...");
+                    _chunksBuffer.Remove(correlationId);
+                }
+            };
+
+            _logger.Information("Inicia o consumo das mensagens...");
+            _channel.BasicConsume(queue: responseQueueName, autoAck: true, consumer: consumer);
+            return tcs;
+        }
+
+        private async Task<string> SendMessageInChunk<T>(T request, string correlationId, byte[] messageBytes, string responseQueueName, TaskCompletionSource<string> tcs)
+        {
+            _logger.Information("Definição dos chunks...");
+            int chunkSize = 128;
+            int totalChunks = (int)Math.Ceiling((double)messageBytes.Length / chunkSize);
+
+            _logger.Information("enviando mensagem em chunks...");
+            try
+            {
+                for (int i = 0; i < totalChunks; i++)
+                {
+                    var chunk = messageBytes.Skip(i * chunkSize).Take(chunkSize).ToArray();
+                    var properties = _channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    properties.CorrelationId = correlationId;
+                    properties.ReplyTo = responseQueueName;
+
+                    var chunkMessage = new ChunkMessageRequest
+                    {
+                        Payload = chunk,
+                        TotalChunks = totalChunks,
+                        CurrentChunk = i + 1,
+                        RequestType = DetermineRequestType(request)
+                    };
+
+                    _channel.BasicPublish(
+                        exchange: "",
+                        routingKey: _configuration["RabbitMqSettings:RequestQueueName"],
+                        basicProperties: properties,
+                        body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(chunkMessage))
+                    );
+                }
+
+                _logger.Information("esperando resposta...");
+
+                var timeout = TimeSpan.FromSeconds(15);
+
+                using (var cts = new CancellationTokenSource(timeout))
+                {
+                    try
+                    {
+                        return await Task.Run(() => tcs.Task, cts.Token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        _logger.Error("Tempo de espera excedido.");
+
+                        var chunkMessageReturn = new ChunkMessageReturn
+                        {
+                            Body = "Tempo de espera excedido.",
+                            StatusCode = 500
+                        };
+                        return JsonConvert.SerializeObject(chunkMessageReturn);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Error($"Erro ao enviar mensagem ou ao processar a resposta: {ex.Message}");
+
+                var chunkMessageReturn = new ChunkMessageReturn
+                {
+                    Body = JsonConvert.SerializeObject(ex.Message),
+                    StatusCode = 500
+                };
+                return JsonConvert.SerializeObject(chunkMessageReturn);
+            }
+            finally
+            {
+                _logger.Information("retira a fila de resposta...");
+                _channel.QueueDelete(responseQueueName);
+            }
+        }
+
+        private ChunkMessageResponse? DeserializeResponseMessage(string message)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<ChunkMessageResponse>(message);
+            }
+            catch (JsonException ex)
+            {
+                _logger.Error(ex, $"Erro ao desserializar a mensagem: {ex.Message}");
+                return null;
+            }
+        }
+
+        private RequestTypeEnum DetermineRequestType<T>(T request)
+        {
+            switch (request)
+            {
+                case CreateClientRequest _:
+                    return RequestTypeEnum.CreateClientRequest;
+
+                case GetClientByIdRequest _:
+                    return RequestTypeEnum.GetClientByIdRequest;
+
+                case GetClientsRequest _:
+                    return RequestTypeEnum.GetClientsRequest;
+
+                case CreateProductRequest _:
+                    return RequestTypeEnum.CreateProductRequest;
+
+                case CreateOrderRequest _:
+                    return RequestTypeEnum.CreateOrderRequest;
+
+                case UpdateClientRequest _:
+                    return RequestTypeEnum.UpdateClientRequest;
+
+                case DeleteClientRequest _:
+                    return RequestTypeEnum.DeleteClientRequest;
+
+                case GetProductByIdRequest _:
+                    return RequestTypeEnum.GetProductByIdRequest;
+
+                case GetProductsRequest _:
+                    return RequestTypeEnum.GetProductsRequest;
+
+                case UpdateProductRequest _:
+                    return RequestTypeEnum.UpdateProductRequest;
+                
+                case DeleteProductRequest _:
+                    return RequestTypeEnum.DeleteProductRequest;
+                
+                case GetOrderValueRequest _:
+                    return RequestTypeEnum.GetOrderValueRequest;
+                
+                case GetQtOrderByClientRequest _:
+                    return RequestTypeEnum.GetQtOrderByClientRequest;
+                
+                case GetOrdersRequest _:
+                    return RequestTypeEnum.GetOrdersRequest;
+                
+                case UpdateOrderRequest _:
+                    return RequestTypeEnum.UpdateOrderRequest;
+                
+                case GetOrderByIdRequest _:
+                    return RequestTypeEnum.GetOrderByIdRequest;
+                
+                case GetListOrderByClientRequest _:
+                    return RequestTypeEnum.GetListOrderByClientRequest;
+                
+                case DeleteOrderRequest _:
+                    return RequestTypeEnum.DeleteOrderRequest;
+                
+                default:
+                    throw new ArgumentException("Tipo de requisição desconhecido", nameof(request));
+            }
+        }
+    }
+}
---
+++ ProjProcessOrders.Messaging/RabbitMqServerService.cs
@@ -0,0 +1,325 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using ProjProcessOrders.Exception.Exceptions;
+using ProjProcessOrders.Messaging.DTOs;
+using ProjProcessOrders.UseCase.Enums;
+using ProjProcessOrders.UseCase.UseCases.CreateClient;
+using ProjProcessOrders.UseCase.UseCases.CreateOrder;
+using ProjProcessOrders.UseCase.UseCases.CreateProduct;
+using ProjProcessOrders.UseCase.UseCases.DeleteClient;
+using ProjProcessOrders.UseCase.UseCases.DeleteProduct;
+using ProjProcessOrders.UseCase.UseCases.GetClientById;
+using ProjProcessOrders.UseCase.UseCases.GetClients;
+using ProjProcessOrders.UseCase.UseCases.GetOrders;
+using ProjProcessOrders.UseCase.UseCases.GetProductById;
+using ProjProcessOrders.UseCase.UseCases.GetProducts;
+using ProjProcessOrders.UseCase.UseCases.UpdateClient;
+using ProjProcessOrders.UseCase.UseCases.UpdateProduct;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Collections.Concurrent;
+using System.Text;
+using ProjProcessOrders.UseCase.UseCases.DeleteOrder;
+using ProjProcessOrders.UseCase.UseCases.GetListOrderByClient;
+using ProjProcessOrders.UseCase.UseCases.GetOrderById;
+using ProjProcessOrders.UseCase.UseCases.GetOrderValue;
+using ProjProcessOrders.UseCase.UseCases.GetQtOrderByClient;
+using ProjProcessOrders.UseCase.UseCases.UpdateOrder;
+
+namespace ProjProcessOrders.Messaging
+{
+    public class RabbitMqServerService : BackgroundService
+    {
+        private readonly IModel _channel;
+        private readonly IConfiguration _configuration;
+        private readonly IMediator _mediator;
+        private readonly IServiceProvider _serviceProvider;
+        //private static readonly ConcurrentDictionary<string, TaskCompletionSource<GetOrdersResponse>> _responseAwaiters = new();
+        private static Dictionary<string, List<ChunkMessageRequest>> _chunksBuffer = new();
+        private readonly string _requestQueue;
+        private readonly Serilog.ILogger _logger;
+
+        //private readonly Dictionary<string, TaskCompletionSource<string>> _callbackMapper = new();
+
+        public RabbitMqServerService(IModel channel, IMediator mediator, IServiceProvider serviceProvider, IConfiguration configuration, Serilog.ILogger logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _channel = channel;
+            _mediator = mediator;
+            _serviceProvider = serviceProvider;
+            _requestQueue = configuration["RabbitMqSettings:RequestQueueName"];
+            _channel.QueueDeclare(queue: _requestQueue, durable: true, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.Information("Iniciando o consumo das mensagens...");
+            var consumer = InitializerConsumer(_channel, _requestQueue, _logger);
+            consumer.Received += async (model, ea) => await HandleMessageAsync(ea, stoppingToken);
+
+            await Task.Delay(-1, stoppingToken);
+        }
+
+        private static EventingBasicConsumer InitializerConsumer(IModel channel, string queueName, Serilog.ILogger logger)
+        {
+            logger.Information("Declarando a fila...");
+            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            logger.Information("Definindo configurações de consumo...");
+            channel.BasicQos(0, 1, false);
+
+            logger.Information("Implementação das configurações de consumo...");
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            return consumer;
+        }
+
+        private async Task HandleMessageAsync(BasicDeliverEventArgs ea, CancellationToken stoppingToken)
+        {
+            _logger.Information("Constroi o objeto chunkMessage...");
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+            ChunkMessageRequest? chunkMessage = DeserializeRequestMessage(message);
+            if (chunkMessage == null) return;
+
+            _logger.Information("Adicionando a mensagem ao buffer...");
+            var correlationId = ea.BasicProperties.CorrelationId;
+            if (!_chunksBuffer.ContainsKey(correlationId))
+                _chunksBuffer[correlationId] = new List<ChunkMessageRequest>();
+
+            _chunksBuffer[correlationId].Add(chunkMessage);
+
+            _logger.Information("Processando as partes da mensagem...");
+            await ProcessChunks(ea, chunkMessage);
+        }
+
+        private ChunkMessageRequest? DeserializeRequestMessage(string message)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<ChunkMessageRequest>(message);
+            }
+            catch (JsonException ex)
+            {
+                _logger.Error(ex, $"Erro ao desserializar a mensagem: {ex.Message}");
+                return null;
+            }
+        }
+
+        private async Task ProcessChunks(BasicDeliverEventArgs ea, ChunkMessageRequest chunkMessage)
+        {
+            var correlationId = ea.BasicProperties.CorrelationId;
+
+            _logger.Information("Verificando se a mensagem chegou ao fim...");
+            if (_chunksBuffer[correlationId].Count == chunkMessage.TotalChunks)
+            {
+                _logger.Information("Preparando a mensagem completa...");
+                var fullMessage = string.Concat(_chunksBuffer[correlationId]
+                    .OrderBy(chunk => chunk.CurrentChunk)
+                    .Select(chunk => Encoding.UTF8.GetString(chunk.Payload)));
+
+                try
+                {
+                    _logger.Information("Processando a mensagem completa...");
+                    await ProcessFullMessage(fullMessage, ea, chunkMessage.RequestType);
+                }
+                catch (System.Exception ex)
+                {
+                    _logger.Error(ex, $"Erro ao processar mensagem: {ex.Message}");
+
+                    ChunkMessageResponse response = new()
+                    {
+                        Payload = Encoding.UTF8.GetBytes(ex.Message),
+                        CurrentChunk = 1,
+                        TotalChunks = 1,
+                        StatusCode = 500
+                    };
+
+                    await SendReplyAsync(ea, response, 500);
+                }
+
+                _logger.Information("Removendo a mensagem do buffer...");
+                _chunksBuffer.Remove(correlationId);
+            }
+
+            _logger.Information("Aprova a mensagem...");
+            _channel.BasicAck(ea.DeliveryTag, false);
+        }
+
+        private async Task ProcessFullMessage(string fullMessage, BasicDeliverEventArgs ea, RequestTypeEnum requestType)
+        {
+            object? request = null;
+
+            _logger.Information("Verificando o tipo de requisição e Deserializando a mensagem...");
+            switch (requestType)
+            {
+                case RequestTypeEnum.CreateClientRequest:
+                    request = JsonConvert.DeserializeObject<CreateClientRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.GetClientsRequest:
+                    request = JsonConvert.DeserializeObject<GetClientsRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.GetClientByIdRequest:
+                    request = JsonConvert.DeserializeObject<GetClientByIdRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.UpdateClientRequest:
+                    request = JsonConvert.DeserializeObject<UpdateClientRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.CreateProductRequest:
+                    request = JsonConvert.DeserializeObject<CreateProductRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.CreateOrderRequest:
+                    request = JsonConvert.DeserializeObject<CreateOrderRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.DeleteClientRequest:
+                    request = JsonConvert.DeserializeObject<DeleteClientRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.GetProductByIdRequest:
+                    request = JsonConvert.DeserializeObject<GetProductByIdRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.GetProductsRequest:
+                    request = JsonConvert.DeserializeObject<GetProductsRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.UpdateProductRequest:
+                    request = JsonConvert.DeserializeObject<UpdateProductRequest>(fullMessage);
+                    break;
+
+                case RequestTypeEnum.DeleteProductRequest:
+                    request = JsonConvert.DeserializeObject<DeleteProductRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.GetOrderValueRequest:
+                    request = JsonConvert.DeserializeObject<GetOrderValueRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.GetQtOrderByClientRequest:
+                    request = JsonConvert.DeserializeObject<GetQtOrderByClientRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.GetOrdersRequest:
+                    request = JsonConvert.DeserializeObject<GetOrdersRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.UpdateOrderRequest:
+                    request = JsonConvert.DeserializeObject<UpdateOrderRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.GetOrderByIdRequest:
+                    request = JsonConvert.DeserializeObject<GetOrderByIdRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.GetListOrderByClientRequest:
+                    request = JsonConvert.DeserializeObject<GetListOrderByClientRequest>(fullMessage);
+                    break;
+                
+                case RequestTypeEnum.DeleteOrderRequest:
+                    request = JsonConvert.DeserializeObject<DeleteOrderRequest>(fullMessage);
+                    break;
+
+                default:
+                    Console.WriteLine($"Tipo de requisição desconhecida");
+                    await SendReplyAsync(ea, $"Tipo de requisição desconhecida", 500);
+                    return;
+            }
+
+            _logger.Information("Inicialização do escopo...");
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var response = default(object);
+
+                _logger.Information("Obtendo o mediador...");
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+                try
+                {
+                    _logger.Information("Processando a requisição...");
+                    response = await mediator.Send(request);
+
+                    _logger.Information("Enviando a resposta...");
+                    await SendReplyAsync(ea, response, 200);
+                }
+                catch (ConflictException ex)
+                {
+                    _logger.Error(ex, $"ConflictException: {ex.Message})");
+
+                    _logger.Information("Enviando a resposta...");
+                    await SendReplyAsync(ea, ex.Message, 409);
+
+                }
+                catch (PreconditionFailedException ex)
+                {
+                    _logger.Error(ex, $"PreconditionFailedException: {JsonConvert.SerializeObject(ex.BadRequestObjectResult.Value)}");
+                       
+                    _logger.Information("Enviando a resposta...");
+                    await SendReplyAsync(ea, ex.BadRequestObjectResult.Value, 412);
+                }
+                catch (System.Exception ex)
+                {
+                    _logger.Error(ex, $"Erro ao processar requisição: {ex.Message})");
+
+                    _logger.Information("Enviando a resposta...");
+                    await SendReplyAsync(ea, ex.Message, 500);
+                }
+            }
+        }
+
+        private async Task SendReplyAsync(BasicDeliverEventArgs ea, object response, int statusCode)
+        {
+            _logger.Information("Criando propriedades da resposta...");
+            var replyProps = _channel.CreateBasicProperties();
+            replyProps.CorrelationId = ea.BasicProperties.CorrelationId;
+
+            _logger.Information("Serializando a resposta...");
+            var replyMessage = JsonConvert.SerializeObject(response);
+            var responseBytes = Encoding.UTF8.GetBytes(replyMessage);
+
+            _logger.Information("Dividindo a resposta em chunks...");
+            const int chunkSize = 128;
+            int totalChunks = (int)Math.Ceiling((double)responseBytes.Length / chunkSize);
+
+            for (int i = 0; i < totalChunks; i++)
+            {
+                _logger.Information($"Criando chunk {i + 1} de {totalChunks}...");
+                int currentChunkSize = Math.Min(chunkSize, responseBytes.Length - (i * chunkSize));
+                byte[] chunkData = new byte[currentChunkSize];
+                Array.Copy(responseBytes, i * chunkSize, chunkData, 0, currentChunkSize);
+
+                _logger.Information($"Criando o objeto ChunkMessageResponse para o chunk {i + 1} de {totalChunks}...");
+                var chunkMessage = new ChunkMessageResponse
+                {
+                    TotalChunks = totalChunks,
+                    CurrentChunk = i + 1,
+                    Payload = chunkData,
+                    StatusCode = statusCode
+                };
+
+                _logger.Information($"Serializando o chunk {i + 1} de {totalChunks}...");
+                var chunkMessageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(chunkMessage));
+
+                _logger.Information($"Enviando o chunk {i + 1} de {totalChunks}...");
+                _channel.BasicPublish(
+                    exchange: "",
+                    routingKey: ea.BasicProperties.ReplyTo,
+                    basicProperties: replyProps,
+                    body: chunkMessageBytes);
+
+                _logger.Information($"Aguardando 10 ms de atraso...");
+                await Task.Delay(10);
+            }
+        }
+
+    }
+}
