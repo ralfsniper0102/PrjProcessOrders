5e88f270fd5d3550c563abe03112b1fd50a13e02
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-12-17 23:48:17 -0300

    clean: removing spaces

--- ProjProcessOrders.Domain/Entities/Product.cs
+++ ProjProcessOrders.Domain/Entities/Product.cs
@@ -7,8 +7,7 @@ namespace ProjProcessOrders.Domain.Entities
         public string ProductName { get; set; }
         public int ProductQuantity { get; set; }
         public decimal ProductPrice { get; set; }
-
-
+        
         public List<OrderProduct> OrderProducts { get; set; }
     }
 }
