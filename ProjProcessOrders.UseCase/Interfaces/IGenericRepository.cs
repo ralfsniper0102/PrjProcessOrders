730f96c7a9d7243146d9820247b728ef18bfd89a
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-12-18 00:04:26 -0300

    put: IGenericRepository

--- ProjProcessOrders.UseCase/Interfaces/IGenericRepository.cs
+++ ProjProcessOrders.UseCase/Interfaces/IGenericRepository.cs
@@ -5,9 +5,10 @@ namespace ProjProcessOrders.UseCase.Interfaces
 {
     public interface IGenericRepository<TEntity, TKeyType> where TEntity : BaseEntity<TKeyType>
     {
-        Task<TEntity> Update(TEntity obj, CancellationToken cancellationToken = default);
+        Task<TEntity> UpdateAsync(TEntity obj, CancellationToken cancellationToken = default);
         IQueryable<TEntity> Queryable(Expression<Func<TEntity, bool>> where, Func<IQueryable<TEntity>, IQueryable<TEntity>> includes = null);
-        Task<TEntity> Insert(TEntity obj, CancellationToken cancellationToken = default);
+        Task<TEntity> InsertAsync(TEntity obj, CancellationToken cancellationToken = default);
         Task SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task DeleteAsync(TEntity obj, CancellationToken cancellationToken = default);
     }
 }
