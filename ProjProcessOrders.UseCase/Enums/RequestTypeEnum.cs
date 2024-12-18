5d64b743fc877b19b88d581ab8052583e24f830a
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-12-18 00:04:00 -0300

    put: enum RequestType

--- ProjProcessOrders.UseCase/Enums/RequestTypeEnum.cs
+++ ProjProcessOrders.UseCase/Enums/RequestTypeEnum.cs
@@ -3,7 +3,22 @@
     public enum RequestTypeEnum
     {
         CreateClientRequest = 1,
+        GetClientsRequest,
+        GetClientByIdRequest,
+        UpdateClientRequest,
+        DeleteClientRequest,
         CreateProductRequest,
-        CreateOrderRequest
+        GetProductByIdRequest,
+        GetProductsRequest,
+        CreateOrderRequest,
+        GetOrderValueRequest,
+        GetQtOrderByClientRequest,
+        UpdateProductRequest,
+        DeleteProductRequest,
+        GetOrdersRequest,
+        UpdateOrderRequest,
+        GetOrderByIdRequest,
+        GetListOrderByClientRequest,
+        DeleteOrderRequest
     }
 }
